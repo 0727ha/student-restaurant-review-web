f6d43c66c1efe4ff9937decc432ddc9835c5d451
hayoung Park <mcd158@naver.com>
2025-03-28 16:13:54 +0900

    setting : 초기 설정

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import * as S from "../styles/Home/HomePageComponentStyle";
+const HomePage = () => {
+	return (
+		<>
+			<S.HomeDiv>test</S.HomeDiv>
+			<S.HomeDiv2>test</S.HomeDiv2>
+			<S.HomeDiv3>test</S.HomeDiv3>
+		</>
+	);
+};
+
+export default HomePage;
